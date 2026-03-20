f4028265f440b4a14a1030695796f3633ea9b5f3
Logan Pearce <lapearce@l-jn2t3m97wy.local>
2026-03-20 10:44:29 -0400

    added reflectx link

--- targets.py
+++ targets.py
@@ -18,18 +18,20 @@ st.set_page_config(
 sidebar_logo = 'images/grip-offwhite.png'
 st.logo(sidebar_logo, size='large')
 
+
 st.sidebar.markdown(
     """<a href="https://reflectx.readthedocs.io" style="text-decoration:none; color:#EEEEEE">
-    <img src=""images/logo-sol.png"" width="25"><br>
-    ReflectX Gas Giant Reflected Light Models</a></div><br><br>"""
-    ,
+    <img src="data:image/png;base64,{}" width="25">
+    </a>""".format(
+        base64.b64encode(open("logo-sol.png", "rb").read()).decode()
+    ),
     unsafe_allow_html=True,
 )
 
 st.sidebar.image("images/logo-sol.png", use_column_width=True)
-st.sidebar.write("""<div style="width:100%;text-align:center;"><a href="https://reflectx.readthedocs.io" style="text-decoration:none; color:#EEEEEE">ReflectX Gas Giant Reflected Light Models</a></div><br><br>""", unsafe_allow_html=True)
+# st.sidebar.write("""<div style="width:100%;text-align:center;"><a href="https://reflectx.readthedocs.io" style="text-decoration:none; color:#EEEEEE">ReflectX Gas Giant Reflected Light Models</a></div><br><br>""", unsafe_allow_html=True)
 
-st.sidebar.write("""<div style="width:100%;text-align:center;"><a href="https://docs.google.com/spreadsheets/d/1sk9wDIVi4uWELL_gSyr7Zb41HZJ27Puyvc1QqWBZFs4/edit?gid=0#gid=0" style="text-decoration:none; color:#EEEEEE">Indirectly Discovered, \n Directly Detected Companions</a></div>""", unsafe_allow_html=True)
+st.sidebar.write("""<div style="width:100%;"><a href="https://docs.google.com/spreadsheets/d/1sk9wDIVi4uWELL_gSyr7Zb41HZJ27Puyvc1QqWBZFs4/edit?gid=0#gid=0" style="text-decoration:none; color:#EEEEEE">Indirectly Discovered, \n Directly Detected Companions</a></div>""", unsafe_allow_html=True)
 
 # left_co, cent_co,last_co = st.columns(3)
 # with cent_co:
